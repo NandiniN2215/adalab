d407db6631356c571f1da1e2366e988ab7eb10ba
omkarcs25 <omkarsadashiv.cs25@bmsce.ac.in>
2026-05-29 10:11:42 +0530

    new files

---
+++ 0_1knapsack.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// Function to return maximum of two numbers
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+// Function to solve 0/1 Knapsack problem
+int knapsack(int W, int wt[], int val[], int n) {
+    int dp[n + 1][W + 1];
+
+    // Build DP table
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+
+            // Base condition
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+
+            // If current item's weight is less than or equal to capacity
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                );
+
+            // Item cannot be included
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main() {
+    int n, W;
+
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    int wt[n], val[n];
+
+    printf("Enter weights of items:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &wt[i]);
+    }
+
+    printf("Enter values of items:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val[i]);
+    }
+
+    printf("Enter knapsack capacity: ");
+    scanf("%d", &W);
+
+    int result = knapsack(W, wt, val, n);
+
+    printf("Maximum value = %d\n", result);
+
+    return 0;
+}
---
+++ floyd's.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define INF 99999
+
+void floydWarshall(int V, int graph[V][V]) {
+    int dist[V][V];
+
+    // Copy input graph to distance matrix
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print result
+    printf("\nShortest distance matrix:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int V;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
+
+    int graph[V][V];
+
+    printf("Enter the adjacency matrix:\n");
+    printf("(Enter %d for INF if no direct edge)\n", INF);
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    floydWarshall(V, graph);
+
+    return 0;
+}
